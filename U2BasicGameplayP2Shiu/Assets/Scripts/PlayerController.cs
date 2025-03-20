a87fbd00d3d2413d49a684200509ee671c982d0c
DxDTitan <728472@student.dusd.net>
2025-03-20 09:55:17 -0700

    lalalalalala
    got to about food flight

---
+++ U2BasicGameplayP2Shiu/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+    // Update is called once per frame
+    void Update()
+    {
+        // keep the player in bounds
+            if (transform.position.x < -xRange)
+            {
+                transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            
+        }
+            if (transform.position.x > xRange)
+        { transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);  
+    }
+}
