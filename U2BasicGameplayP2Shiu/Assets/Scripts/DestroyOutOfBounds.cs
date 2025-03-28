0a8831fd504a7e23b1512c65cd1137180890e69d
DxDTitan <728472@student.dusd.net>
2025-03-28 09:57:21 -0700

    fixed all spawning issues

--- U2BasicGameplayP2Shiu/Assets/Scripts/DestroyOutOfBounds.cs
+++ U2BasicGameplayP2Shiu/Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
+    private float topBound = 30;
+    private float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    private float topBound = 50;
-    private float lowerBound = -30;
+    
     // Update is called once per frame
     void Update()
     {
         if (transform.position.z > topBound)
-        { Destroy(gameObject); }
-        else if (transform.position.z < lowerBound)
-        Debug.Log("Game Over!");
-        { Destroy(gameObject); }
+        {
+            Destroy(gameObject);
+
+        } else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
